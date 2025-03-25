ff797b6a76ef8674db3cac3404694a7c11b1c131
DiegoDiazHL <diegodiazhellinl@outlook.com>
2025-03-25 13:58:58 +0100

    clases

---
+++ GranjeroBuscaEsposa/src/Terrenos/Granja.java
@@ -0,0 +1,25 @@
+package Terrenos;
+
+public class Granja extends Terreno{
+    private boolean limpio;
+
+    public Granja(Integer tamano, Integer capacidad, Integer precio, String tipo, boolean limpio) {
+        super(tamano, capacidad, precio, tipo);
+        this.limpio = limpio;
+    }
+
+    public boolean isLimpio() {
+        return limpio;
+    }
+
+    public void setLimpio(boolean limpio) {
+        this.limpio = limpio;
+    }
+
+    @Override
+    public String toString() {
+        return "Granja{" +
+                "limpio=" + limpio +
+                '}';
+    }
+}
